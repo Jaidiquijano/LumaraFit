d41eb30f6f31b26d66d0d08a81a2fb3ed0ffb1c8
JaidiQuijano <jaidy33@hotmail.es>
2026-04-05 02:16:18 +0200

    se realizo la conecion con la base de datos mongodb atlas y mi proyecto lumarafit

---
+++ src/main/java/LumaraFit/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package LumaraFit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Desactiva la protección CSRF para poder hacer POST
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Permite TODAS las peticiones sin contraseña
+                );
+        return http.build();
+    }
+}
